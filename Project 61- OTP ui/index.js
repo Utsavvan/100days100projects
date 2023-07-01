08d8983054a4683f63b6b97904fd527f1390574d
Utsavvan <utsav1pariya@outlook.com>
2023-07-02 03:11:32 +0530

    Project 61- OTP ui

---
+++ Project 61- OTP ui/index.js
@@ -0,0 +1,14 @@
+function nextFocus(ele){
+
+    let number = Number(ele.dataset.id);
+    document.getElementsByClassName("number")[number].focus();
+}
+
+// nextFocus(2);
+
+let allFields = document.getElementsByClassName("number");
+
+
+
+
+
